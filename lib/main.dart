c3d49f122552d53a711545d31e812580c1b2b09a
qendev <qendev00@gmail.com>
2021-03-17 17:40:39 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyHome());
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calenda Example',
+      home: MyCalenda(),
+    );
+  }
+}
+
+
+class MyCalenda extends StatefulWidget {
+  @override
+  _MyCalendaState createState() => _MyCalendaState();
+}
+
+class _MyCalendaState extends State<MyCalenda> {
+  //use DateTime object to get current.
+  DateTime currentDate = DateTime.now();
+
+  //create a new DatePicker
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime pickedDate = await showDatePicker(
+        context: context,
+        initialDate: currentDate,
+        firstDate: DateTime(1900),
+        lastDate: DateTime(5000));
+    if (pickedDate != null && pickedDate != currentDate)
+      setState(() {
+        currentDate = pickedDate;
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //creating the ui
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DatePicker"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(currentDate.toString()),
+            SizedBox(
+              height: 20.0,
+            ),
+            RaisedButton(
+              onPressed: () => _selectDate(context),
+              child: Text('Select date'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
